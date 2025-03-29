ea9f423cb7d8de8787fc97d0c350a484686f4ad0
hyemin <139746386+hyeminboo@users.noreply.github.com>
2025-03-29 18:14:28 +0900

    Time: 507 ms (24.96%), Space: 17.6 MB (81.66%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -2,12 +2,12 @@ class Solution:
     def arrangeCoins(self, n: int) -> int:
         if n == 1:
             return 1
-            
+
         n_copy = n + 1
         ans = 0
 
         for i in range(1, n_copy):
             n -= i
-            if n <= 0:
-                return ans        
+            if n < 0:
+                return ans       
             ans += 1
