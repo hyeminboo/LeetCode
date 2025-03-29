f1f9b1097cd96b2a3c7103c0dfb62be3dbf77b85
hyemin <139746386+hyeminboo@users.noreply.github.com>
2025-03-29 18:15:22 +0900

    Time: 1 ms (73.08%), Space: 17.7 MB (81.66%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -1,13 +1,22 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        if n == 1:
-            return 1
+        """
+        각 층 k 에서 필요한 코인 수 : k * (k + 1) / 2
+        n의 mid에서 구한 코인 수 < n이라면 left 증가, 아니라면 right 감소
+        """
 
-        n_copy = n + 1
-        ans = 0
+        left, right = 1, n
 
-        for i in range(1, n_copy):
-            n -= i
-            if n < 0:
-                return ans       
-            ans += 1
+        while left <= right:
+            mid = (left + right) // 2
+            num = mid * (mid + 1) // 2
+
+            if n == num:
+                return mid
+            
+            if n > num:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return right
